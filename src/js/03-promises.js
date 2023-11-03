f927c494cafc85a0215687e8bb53e4fde46f91c9
Max Samsonenko <samsonenko.maxwork@gmail.com>
2023-11-03 12:12:52 +0200

    task 3 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,62 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+//iziToast options
+const errorMessage = {
+  title: '',
+  message: '',
+  position: 'topRight',
+};
+const successMessage = {
+  title: '',
+  message: '',
+  position: 'topRight',
+};
+
+//get form element
+const form = document.querySelector('.form');
+//an object for storing form input values
+const userInput = {};
+//variable for storing promise number
+let promiseCounter = 0;
+//add event listeners to form element
+form.addEventListener('input', handleUserInput);
+form.addEventListener('submit', handleFormSubmit);
+//get user input values and store them in userInput object
+function handleUserInput(e) {
+  userInput[e.target.name] = Number(e.target.value);
+}
+//handle form submit
+function handleFormSubmit(e) {
+  e.preventDefault();
+  let step = userInput.delay;
+  //create promises according to amount value
+  for (let i = 0; i < userInput.amount; i += 1) {
+    promiseCounter += 1;
+    createPromise(promiseCounter, step).then(onSuccess).catch(onError);
+    step += userInput.step;
+  }
+}
+//callback function if promise fulfilled
+function onSuccess({ position, delay }) {
+  successMessage.message = `Fulfilled promise ${position} in ${delay}ms`;
+  iziToast.success(successMessage);
+}
+//callback function if promise rejected
+function onError({ position, delay }) {
+  errorMessage.message = `Rejected promise ${position} in ${delay}ms`;
+  iziToast.error(errorMessage);
+}
+//create promise
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
